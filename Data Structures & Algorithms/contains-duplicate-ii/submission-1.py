4122d377d33de932488a6e20ab11502f1875a4d4
Sanchit Gnawali <31901263+sanchitgnawali@users.noreply.github.com>
2026-07-17 23:07:47 +1000

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.py
@@ -0,0 +1,11 @@
+# Using hashmap
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+
+        for i in range(len(nums)):
+            if nums[i] in hashmap and abs(i - hashmap[nums[i]]) <= k:
+                return True
+            hashmap[nums[i]] = i
+
+        return False
